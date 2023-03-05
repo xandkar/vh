11d7c8794af164131641aae8fea0a50372514d55
Siraaj Khandkar <siraaj@khandkar.net>
2023-03-05 15:11:41 -0500

    Allow specifying color on CLI

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,50 @@
 use std::io::{Read, Write};
 
 use anyhow::{anyhow, Result};
+use clap::Parser;
+use pixelate::Color;
 
-fn blockify<W: Write>(input: &[u8], output: W) -> Result<()> {
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(long = "fg-rgb", default_value = "0-0-0")]
+    fg_rgb: String,
+
+    #[arg(long = "bg-rgb", default_value = "255-255-255")]
+    bg_rgb: String,
+
+    #[arg(long = "fg-alpha", default_value = "255")]
+    fg_alpha: u8,
+
+    #[arg(long = "bg-alpha", default_value = "0")]
+    bg_alpha: u8,
+}
+
+fn blockify<W: Write>(input: &[u8], output: W, fg: Color, bg: Color) -> Result<()> {
     // TODO Re-implement blockies.
     let gen = blockies::Classic {
-        color: Some(pixelate::BLACK),
+        color: Some(fg),
         size: 8,
         scale: 8,
-        background_color: None,
+        background_color: Some(bg),
     };
     gen.create_icon(output, input).map_err(|e| anyhow!("blockies failure: {:?}", e))
 }
 
+fn color_parse(rgb_string: &str, alpha: u8) -> Result<Color> {
+    let mut rgb: Vec<u8> = Vec::new();
+    for num in rgb_string.split('-') {
+        rgb.push(num.parse()?)
+    }
+    match rgb[..] {
+        [r, g, b] => Ok(Color::Rgba(r,  g, b, alpha)),
+        _ => Err(anyhow!("Invalid number of RGB components in: {:?}", rgb_string))
+    }
+}
+
 fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let fg = color_parse(&cli.fg_rgb, cli.fg_alpha)?;
+    let bg = color_parse(&cli.bg_rgb, cli.bg_alpha)?;
     let input = {
         let mut buf = Vec::new();
         for byte_result in std::io::stdin().bytes() {
@@ -22,6 +53,6 @@ fn main() -> Result<()> {
         buf
     };
     let mut output = std::io::stdout();
-    blockify(&input[..], &mut output)?;
+    blockify(&input[..], &mut output, fg, bg)?;
     Ok(())
 }
